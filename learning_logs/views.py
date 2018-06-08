aef2baf024d559976b06d2b7ef2ca4177deb0360
Pavel Kozlov <pavelcauselov@gmail.com>
2018-06-08 12:15:10 +0300

    templates

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
@@ -20,7 +20,7 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic, request.user)
 
     entries = topic.entry_set.order_by('-date_added')
@@ -44,7 +44,7 @@ def new_topic(request):
 
 @login_required
 def new_entry(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic, request.user)
 
     if request.method != 'POST':
@@ -63,7 +63,7 @@ def new_entry(request, topic_id):
 
 @login_required
 def edit_entry(request, entry_id):
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     check_topic_owner(topic, request.user)
 
